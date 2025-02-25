ba4651e5b02652ed23e4a8d6a16aa7950d47c762
Starlight_77 <jammyabey77@gmail.com>
2025-02-26 02:08:33 +0530

    Add files via upload
    Calculating Hypotenuse "c"

---
+++ Pythagorean Theorem.py
@@ -0,0 +1,11 @@
+# Pythagorean Theorem 
+
+a = int(input('Enter a: ')) # Take as 2 for example
+b = int(input('Enter b: ')) # Take as 4 for example
+
+c = (a**2 + b**2) ** 0.5 # "c" is hypotenuse 
+
+print(c) # Answer should be 4.47213595499958
+
+# Can put any whole number as for "a" and "b" values, you will get different answers as for "c" according to the values put
+
