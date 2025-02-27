0ee44ef30734fa6218e575c355d2885818ae5c3a
Starlight_77 <jammyabey77@gmail.com>
2025-02-27 21:08:21 +0530

    Add files via upload
    If your grade is greater than or equal to 55 you have passed! But if less you have unfortunately failed :(

---
+++ Grades.py
@@ -0,0 +1,11 @@
+# Grades
+
+grade = int(input('grade: '))
+
+if grade >= 55:
+  print('You passed.')
+else:
+  print('You failed.')
+
+# If the grade is 55 or more you should get as "You passed".
+# if the grade is less than 55 you should get as "You failed". 
