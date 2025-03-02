279ba3e3d68c1c92b66f049de61fa08f4a0ae409
Starlight_77 <jammyabey77@gmail.com>
2025-03-02 13:15:49 +0530

    Add files via upload

---
+++ Pythagorean Theorem.py
@@ -0,0 +1,12 @@
+# Pythagorean Theorem 
+
+a = int(input('Enter a: '))  # Take as 2 for example
+b = int(input('Enter b: '))  # Take as 4 for example
+
+c = (a**2 + b**2) ** 0.5  # "c" is hypotenuse 
+
+print(c)  # Answer should be 4.47213595499958
+
+# Can put any whole number for "a" and "b" values
+# You will then get different answers for "c" according to the values put
+
